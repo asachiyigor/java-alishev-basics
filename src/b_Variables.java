0acedd869e7b799c438cc8bf6b68d0eacd4f9fa9
asachiyigor <asachiyigor@gmail.com>
2022-03-16 19:16:56 +0300

    1-4 lesson

---
+++ src/b_Variables.java
@@ -0,0 +1,18 @@
+public class b_Variables {
+    public static void main(String[] args) {
+
+        int myInt= 444; // -2147483648 +2147483647
+        short myShort = 3266; // -32768 +32767
+        long myLong = 2323223; // -9223372036854775808l +9223372036854775807l
+
+        double myDouble = 234.4; // %.0f для целого числа
+        float myfloat = 234.4f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100; // -128 + 127
+
+        System.out.println(myInt);
+    }
+}
---
+++ src/c_String.java
@@ -0,0 +1,17 @@
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
+public class c_String {
+    public static void main(String [] args) {
+
+        int x = 5;
+        String s = "Hello";
+        String space = " ";
+        String name = "Igor";
+        System.out.println(s + space + name);
+        System.out.println("My number is " + x);
+
+
+
+    }
+}
